9c1b90982940a9029cfacfab7909dfe7bd67d7b2
HugoCardi <hugo.cardi@gmail.com>
2019-08-30 13:25:47 -0500

    08/30/2019

--- ScheduleMaker/ViewController.swift
+++ ScheduleMaker/ViewController.swift
@@ -17,5 +17,29 @@ class ViewController: UIViewController {
 		// Do any additional setup after loading the view.
 	}
 
+	override func viewWillAppear(_ animated: Bool) {
+		guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+				return
+		}
+		let managedContext = appDelegate.persistentContainer.viewContext
+		// 2
+				
+		func save(name: String) {
+
+			let entity = NSEntityDescription.entity(forEntityName: "Student",in: managedContext)!
+			let dude = NSManagedObject(entity: entity, insertInto: managedContext)
+						dude.setValue(name, forKeyPath: "student_name")
+			// 4
+			do {
+				try managedContext.save()
+				//people.append(person)
+			} catch let error as NSError {
+				print("Could not save. \(error), \(error.userInfo)")
+			}
+		}
+			
+		
+	}
+
 }
 
